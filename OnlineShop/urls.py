eef4602e341ec5cf2d1ab0cd9b804e1ff2ce792e
Arshia-Haeri-Mehrizi <haeri.arshia@gmail.com>
2025-03-14 12:53:04 +0330

    Basic Templating

--- OnlineShop/urls.py
+++ OnlineShop/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('website.urls')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('assets/icon/favicon.ico'))),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
